516d478fc45deed655fcca2f6ed68114f9ddf7ff
Naman0508 <nsadhikari21@gmail.com>
2023-03-25 23:42:50 +0530

    pp1

--- src/main/java/com/driver/services/ParkingLotService.java
+++ src/main/java/com/driver/services/ParkingLotService.java
@@ -1,7 +1,12 @@
 package com.driver.services;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Spot;
+
+import java.util.List;
+
 public interface ParkingLotService {
-    
+
     void deleteSpot(int spotId);
 
     Spot updateSpot(int parkingLotId, int spotId, int pricePerHour);
@@ -11,4 +16,6 @@ public interface ParkingLotService {
     ParkingLot addParkingLot(String name, String address);
 
     Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour);
+
+    List<Spot> findAllSpotsOfAParkingLot(int parkingLotId);
 }
